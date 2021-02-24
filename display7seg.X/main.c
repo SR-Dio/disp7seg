0dcf0f81fb6c11cd23603eaf092c542b4f41d3fd
19179166 <19179166@DSN1271158607.cfp127aluno.local>
2021-02-24 16:37:53 -0300

    alteracao do vetor do main para o display7segh

--- display7seg.X/main.c
+++ display7seg.X/main.c
@@ -11,10 +11,7 @@
 #include "delay.h"
 #include "display7seg_init.h"
 
-char  vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
-                    0x66, 0x6D, 0x7D, 0x07,
-                    0x7F, 0x6F, 0x77, 0x7C,
-                    0x39, 0x5E, 0x79, 0x71};
+
     
 
     
@@ -26,7 +23,7 @@ void main(void)
     
     while (1)
     {
-       display7seg( vetor[cont] );
+       display7seg(cont);
        delay (1000);
        cont++;
        if( cont >= 16)
